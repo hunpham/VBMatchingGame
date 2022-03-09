80cd45c956fdcdfb3884f7ff41c49574fe88ea18
hunpham <36614439+hunpham@users.noreply.github.com>
2022-03-09 04:34:07 -0700

    Add files via upload

---
+++ MatchingGame/MainForm.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.closeCardTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.tableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.label16 = New System.Windows.Forms.Label()
+        Me.label15 = New System.Windows.Forms.Label()
+        Me.label14 = New System.Windows.Forms.Label()
+        Me.label13 = New System.Windows.Forms.Label()
+        Me.label12 = New System.Windows.Forms.Label()
+        Me.label11 = New System.Windows.Forms.Label()
+        Me.label10 = New System.Windows.Forms.Label()
+        Me.label9 = New System.Windows.Forms.Label()
+        Me.label8 = New System.Windows.Forms.Label()
+        Me.label7 = New System.Windows.Forms.Label()
+        Me.label6 = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.tableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'closeCardTimer
+        '
+        Me.closeCardTimer.Interval = 750
+        '
+        'tableLayoutPanel1
+        '
+        Me.tableLayoutPanel1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset
+        Me.tableLayoutPanel1.ColumnCount = 4
+        Me.tableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.Controls.Add(Me.label16, 3, 3)
+        Me.tableLayoutPanel1.Controls.Add(Me.label15, 2, 3)
+        Me.tableLayoutPanel1.Controls.Add(Me.label14, 1, 3)
+        Me.tableLayoutPanel1.Controls.Add(Me.label13, 0, 3)
+        Me.tableLayoutPanel1.Controls.Add(Me.label12, 3, 2)
+        Me.tableLayoutPanel1.Controls.Add(Me.label11, 2, 2)
+        Me.tableLayoutPanel1.Controls.Add(Me.label10, 1, 2)
+        Me.tableLayoutPanel1.Controls.Add(Me.label9, 0, 2)
+        Me.tableLayoutPanel1.Controls.Add(Me.label8, 3, 1)
+        Me.tableLayoutPanel1.Controls.Add(Me.label7, 2, 1)
+        Me.tableLayoutPanel1.Controls.Add(Me.label6, 1, 1)
+        Me.tableLayoutPanel1.Controls.Add(Me.label5, 0, 1)
+        Me.tableLayoutPanel1.Controls.Add(Me.label4, 3, 0)
+        Me.tableLayoutPanel1.Controls.Add(Me.label3, 2, 0)
+        Me.tableLayoutPanel1.Controls.Add(Me.label2, 1, 0)
+        Me.tableLayoutPanel1.Controls.Add(Me.label1, 0, 0)
+        Me.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tableLayoutPanel1.Location = New System.Drawing.Point(23, 69)
+        Me.tableLayoutPanel1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.tableLayoutPanel1.Name = "tableLayoutPanel1"
+        Me.tableLayoutPanel1.RowCount = 4
+        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.tableLayoutPanel1.Size = New System.Drawing.Size(577, 498)
+        Me.tableLayoutPanel1.TabIndex = 0
+        '
+        'label16
+        '
+        Me.label16.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label16.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label16.Location = New System.Drawing.Point(435, 374)
+        Me.label16.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label16.Name = "label16"
+        Me.label16.Size = New System.Drawing.Size(136, 122)
+        Me.label16.TabIndex = 15
+        Me.label16.Text = "c"
+        Me.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label15
+        '
+        Me.label15.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label15.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label15.Location = New System.Drawing.Point(292, 374)
+        Me.label15.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label15.Name = "label15"
+        Me.label15.Size = New System.Drawing.Size(133, 122)
+        Me.label15.TabIndex = 14
+        Me.label15.Text = "c"
+        Me.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label14
+        '
+        Me.label14.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label14.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label14.Location = New System.Drawing.Point(149, 374)
+        Me.label14.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label14.Name = "label14"
+        Me.label14.Size = New System.Drawing.Size(133, 122)
+        Me.label14.TabIndex = 13
+        Me.label14.Text = "c"
+        Me.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label13
+        '
+        Me.label13.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label13.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label13.Location = New System.Drawing.Point(6, 374)
+        Me.label13.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label13.Name = "label13"
+        Me.label13.Size = New System.Drawing.Size(133, 122)
+        Me.label13.TabIndex = 12
+        Me.label13.Text = "c"
+        Me.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label12
+        '
+        Me.label12.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label12.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label12.Location = New System.Drawing.Point(435, 250)
+        Me.label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label12.Name = "label12"
+        Me.label12.Size = New System.Drawing.Size(136, 122)
+        Me.label12.TabIndex = 11
+        Me.label12.Text = "c"
+        Me.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label11
+        '
+        Me.label11.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label11.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label11.Location = New System.Drawing.Point(292, 250)
+        Me.label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label11.Name = "label11"
+        Me.label11.Size = New System.Drawing.Size(133, 122)
+        Me.label11.TabIndex = 10
+        Me.label11.Text = "c"
+        Me.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label10
+        '
+        Me.label10.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label10.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label10.Location = New System.Drawing.Point(149, 250)
+        Me.label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label10.Name = "label10"
+        Me.label10.Size = New System.Drawing.Size(133, 122)
+        Me.label10.TabIndex = 9
+        Me.label10.Text = "c"
+        Me.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label9
+        '
+        Me.label9.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label9.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label9.Location = New System.Drawing.Point(6, 250)
+        Me.label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label9.Name = "label9"
+        Me.label9.Size = New System.Drawing.Size(133, 122)
+        Me.label9.TabIndex = 8
+        Me.label9.Text = "c"
+        Me.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label8
+        '
+        Me.label8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label8.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label8.Location = New System.Drawing.Point(435, 126)
+        Me.label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label8.Name = "label8"
+        Me.label8.Size = New System.Drawing.Size(136, 122)
+        Me.label8.TabIndex = 7
+        Me.label8.Text = "c"
+        Me.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label7
+        '
+        Me.label7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label7.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label7.Location = New System.Drawing.Point(292, 126)
+        Me.label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label7.Name = "label7"
+        Me.label7.Size = New System.Drawing.Size(133, 122)
+        Me.label7.TabIndex = 6
+        Me.label7.Text = "c"
+        Me.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label6
+        '
+        Me.label6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label6.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label6.Location = New System.Drawing.Point(149, 126)
+        Me.label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label6.Name = "label6"
+        Me.label6.Size = New System.Drawing.Size(133, 122)
+        Me.label6.TabIndex = 5
+        Me.label6.Text = "c"
+        Me.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label5
+        '
+        Me.label5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label5.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label5.Location = New System.Drawing.Point(6, 126)
+        Me.label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(133, 122)
+        Me.label5.TabIndex = 4
+        Me.label5.Text = "c"
+        Me.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label4
+        '
+        Me.label4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label4.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label4.Location = New System.Drawing.Point(435, 2)
+        Me.label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(136, 122)
+        Me.label4.TabIndex = 3
+        Me.label4.Text = "c"
+        Me.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label3
+        '
+        Me.label3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label3.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label3.Location = New System.Drawing.Point(292, 2)
+        Me.label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(133, 122)
+        Me.label3.TabIndex = 2
+        Me.label3.Text = "c"
+        Me.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label2
+        '
+        Me.label2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label2.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label2.Location = New System.Drawing.Point(149, 2)
+        Me.label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(133, 122)
+        Me.label2.TabIndex = 1
+        Me.label2.Text = "c"
+        Me.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label1
+        '
+        Me.label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.label1.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label1.Location = New System.Drawing.Point(6, 2)
+        Me.label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(133, 122)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "c"
+        Me.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(623, 590)
+        Me.Controls.Add(Me.tableLayoutPanel1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "MainForm"
+        Me.Padding = New System.Windows.Forms.Padding(23, 69, 23, 23)
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Style = MetroFramework.MetroColorStyle.White
+        Me.Text = "Matching Game"
+        Me.TextAlign = System.Windows.Forms.VisualStyles.HorizontalAlign.Center
+        Me.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.tableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents closeCardTimer As Timer
+    Friend WithEvents tableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents label16 As Label
+    Friend WithEvents label15 As Label
+    Friend WithEvents label14 As Label
+    Friend WithEvents label13 As Label
+    Friend WithEvents label12 As Label
+    Friend WithEvents label11 As Label
+    Friend WithEvents label10 As Label
+    Friend WithEvents label9 As Label
+    Friend WithEvents label8 As Label
+    Friend WithEvents label7 As Label
+    Friend WithEvents label6 As Label
+    Friend WithEvents label5 As Label
+    Friend WithEvents label4 As Label
+    Friend WithEvents label3 As Label
+    Friend WithEvents label2 As Label
+    Friend WithEvents label1 As Label
+End Class
